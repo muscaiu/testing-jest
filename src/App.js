bf3e0c48dcf6b071930c40372505b8a8cd2d89f4
Cris <muscaiu@yahoo.com>
2017-09-19 07:49:26 +0300

    shalow testing a Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header'
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <Header />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
