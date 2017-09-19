35a9f2f265e1cb24eed3f3c54b9aeb400701fa5b
Cris <muscaiu@yahoo.com>
2017-09-19 07:14:35 +0300

    importing and testing a simple function

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { sum } from './sum'
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3)
+})
