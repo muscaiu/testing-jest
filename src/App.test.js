f6e247f1dee3de95e342f003f447d766b9137dfa
Cris <muscaiu@yahoo.com>
2017-09-19 07:21:24 +0300

    testing shallow from enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme'
 
 import { sum } from './sum'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-});
+})
+
+it('renders shallow without crashing', () => {
+  shallow(<App />)
+})
 
 it('sums numbers', () => {
   expect(sum(1, 2)).toEqual(3)
